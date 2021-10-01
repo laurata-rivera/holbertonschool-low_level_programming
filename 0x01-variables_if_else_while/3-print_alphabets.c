7b5a6963985885b2c103b17e42df78b1430225f1
Laura Rivera <laurata.rivera@gmail.com>
2021-10-01 15:47:12 -0700

    C: variable, while, putchar, putchar

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+/**
+ * n: use ASCII, a lowercase is 97
+ * m: use ASCII, A upcase is 65
+ */
+	int n = 97;
+	int m = 65;
+	while (n < 123)
+	{
+		putchar(n);
+		n++
+	}
+	while (m < 65)
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
+	return (0);
+}
