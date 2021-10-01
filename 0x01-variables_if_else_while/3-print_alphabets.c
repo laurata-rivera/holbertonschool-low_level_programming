b2dd7529110bb92f8b1c2ce1436c53e891a39f1c
Laura Rivera <laurata.rivera@gmail.com>
2021-10-01 15:55:12 -0700

    C: print alphabets L&U with putchar

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,22 +7,21 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-
-/**
- * n: use ASCII, a lowercase is 97
- * m: use ASCII, A upcase is 65
- */
+	/**
+	 * n: use ASCII, a lowercase is 97
+	 * m: use ASCII, A upcase is 65
+	 */
 	int n = 97;
 	int m = 65;
+
 	while (n < 123)
 	{
 		putchar(n);
-		n++
+		n++;
 	}
-	while (m < 65)
+	while (m < 91)
 	{
 		putchar(m);
 		m++;
