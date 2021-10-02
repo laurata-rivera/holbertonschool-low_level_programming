f0d057c22508d35df69a9355d157a20c643d398d
Laura Rivera <laurata.rivera@gmail.com>
2021-10-01 17:33:36 -0700

    C: alphabet in reverse, --

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/**
+	 * n: use ASCII, z lowercase is 122
+	 */
+	int n = 122;
+
+	while (n > 96)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
