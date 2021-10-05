39173ed58a30180e4b416f38cd8882b94b6432e8
Laura Rivera <laurata.rivera@gmail.com>
2021-10-05 09:58:17 -0700

    C: addition of integers (+)

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add -returns the sum of two integers
+ * @x: Interger
+ * @y: Interger
+ * Return: to z, the result
+ */
+int add(int x, int y)
+{
+	int z;
+
+	z = (x + y);
+	return (z);
+}
