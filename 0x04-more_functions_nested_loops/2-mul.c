dd3e2e3efd00d5b8a411e26704d9ec31c51523a6
Laura Rivera <laurata.rivera@gmail.com>
2021-10-08 06:06:23 -0700

    C: multiplication integers, with any loop

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: Input parameter integers
+ * @b: Input parameter integers
+ *
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appopiatly
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
